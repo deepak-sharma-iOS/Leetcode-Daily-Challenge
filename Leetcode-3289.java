a0282365e1ee7aa45a1d3ada81e6769cd762d8b5
deepak-sharma-iOS <90516407+deepak-sharma-iOS@users.noreply.github.com>
2025-10-31 08:15:41 +0530

    Leetcode-3289.java

---
+++ Leetcode-3289.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        int[] result = new int[2];
+        boolean firstFound = false;
+
+        for (int n : nums) {
+            if (!uniqueNumbers.add(n)) {
+                if (firstFound) {
+                    result[1] = n;
+                    return result;
+                } else {
+                    firstFound = true;
+                    result[0] = n;
+                }
+            }
+        }
+
+        return result;
+    }
+}
